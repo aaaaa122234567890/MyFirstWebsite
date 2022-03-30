7fcd5137323c3bfeea40d479a2e691611037f1f3
aaaaa122234567890 <102689290+aaaaa122234567890@users.noreply.github.com>
2022-03-30 19:42:53 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,14 +1,9 @@
-<html>
-	<head>
-		<title>My first PHP website</title>
-	</head>
-	<body>
-		<h2>Login Page</h2>
-		<a href="index.php">Click here to go back</a><br/><br/>
-		<form action="checklogin.php" method="post">
-			Enter Username: <input type="text" name="username" required="required"/> <br/>
-			Enter Password: <input type="password" name="password" required="required" /> <br/>
-			<input type="submit" value="Login"/>
-		</form>
-	</body>
+‹htmL>
+<body>
+«form action="pershendetje. php" method="get">
+Emri: <input type="text" name="emri"> </br>
+Email: <input type="text" name="email"> </br>
+<input type="submit">
+</form>
+</body>
 </html>
